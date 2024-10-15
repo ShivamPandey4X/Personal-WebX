fc9e8f94db283d7bcbc6cbff7ddfd87c7dc13f7a
Shivam Kr Pandey <shivam841425374@gmail.com>
2024-10-15 10:56:33 +0530

    First Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development",  "Full Stack Web Development"],
+    strings: ["frontend development", "Cyber Security", "Cloud Enthusiast"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
